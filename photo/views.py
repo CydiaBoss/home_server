02676a80571cae6a4d042cc93cbf127451e96e0d
Andrew Wang <andrew_wang2002@hotmail.com>
2023-03-09 11:16:18 -0500

    UPD -> setup demo api

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+class PhotoView(APIView):
+
+    def get(self, request : Request):
+
+        return Response({"success" : "ok"})
