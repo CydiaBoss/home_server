acf6450eeafcaa22d1278315af5b0da54c933572
Andrew Wang <andrew_wang2002@hotmail.com>
2023-04-24 21:31:17 -0400

    UPD admin panels

--- media/admin.py
+++ media/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from media.models import *
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
