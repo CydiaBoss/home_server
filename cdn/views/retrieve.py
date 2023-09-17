07e6db78c7ccba660692918dd00554ed00f09d8c
Andrew Wang <andrew_wang2002@hotmail.com>
2023-09-16 20:16:50 -0400

    UPD -> move logic to common app

--- cdn/views/retrieve.py
+++ cdn/views/retrieve.py
@@ -1,15 +1,13 @@
 import mimetypes
-import posixpath
-from pathlib import Path
 
-from django.conf import settings
 from django.http.request import HttpRequest
 from django.http import FileResponse, Http404, HttpResponseNotModified
-from django.utils._os import safe_join
 from django.utils.http import http_date
 from django.utils.translation import gettext as _
 from django.views import View
 from django.views.static import was_modified_since
+
+from common.utils import get_filepath
 
 class MediaRetrieveView(View):
     
@@ -23,24 +21,23 @@ class MediaRetrieveView(View):
         Route: [GET] /cdn/media/:path_to_file/
         '''
         # Get Path
-        path = posixpath.normpath(path).lstrip("/")
-        fullpath = Path(safe_join(settings.MEDIA_ROOT, path))
+        filepath = get_filepath(path=path)
 
         # Fail Directory or Not Found
-        if fullpath.is_dir() or not fullpath.exists():
-            raise Http404(_("“%(path)s” is not accessible") % {"path": fullpath})
+        if filepath.is_dir() or not filepath.exists():
+            raise Http404(_("“%(path)s” is not accessible") % {"path": filepath})
         
         # Respect the If-Modified-Since header.
-        statobj = fullpath.stat()
+        statobj = filepath.stat()
         if not was_modified_since(
             request.META.get("HTTP_IF_MODIFIED_SINCE"), statobj.st_mtime
         ):
             return HttpResponseNotModified()
         
         # Generate Static View
-        content_type, encoding = mimetypes.guess_type(str(fullpath))
+        content_type, encoding = mimetypes.guess_type(str(filepath))
         content_type = content_type or "application/octet-stream"
-        response = FileResponse(fullpath.open("rb"), content_type=content_type)
+        response = FileResponse(filepath.open("rb"), content_type=content_type)
         response.headers["Last-Modified"] = http_date(statobj.st_mtime)
         if encoding:
             response.headers["Content-Encoding"] = encoding
--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,11 @@
+import posixpath
+from pathlib import Path
+
 from typing import Type, TypeVar, Union
 
+from django.conf import settings
 from django.db.models import Model, Q
+from django.utils._os import safe_join
 
 # Django Model TypeVar
 T = TypeVar("T", bound=Model)
@@ -18,3 +23,16 @@ def get_or_none(model : Type[T], *args : Q, **kwargs) -> Union[T, None]:
         A Django model or None
     """
     return model.objects.filter(*args, **kwargs).first()
+
+def get_filepath(path : str) -> Path:
+    """
+    Retrieves the path object of a static file
+
+    Args:
+        path: Filepath of file within MEDIA_ROOT
+
+    Returns:
+        A Filepath object of the file
+    """
+    path = posixpath.normpath(path).lstrip("/")
+    return Path(safe_join(settings.MEDIA_ROOT, path))
