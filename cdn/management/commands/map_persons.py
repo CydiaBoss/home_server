350515f16901793a1d1ab3d7c62302e21a4c402c
Andrew Wang <andrew_wang2002@hotmail.com>
2025-05-19 17:23:35 -0400

    add support for sub dir with multiple layers

--- cdn/management/commands/map_persons.py
+++ cdn/management/commands/map_persons.py
@@ -22,10 +22,15 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		self.stdout.write('Started scanning the MEDIA_ROOT')
 
+		# Subdir msg
+		if options["dir"] != "":
+			self.stdout.write(f'Sub directory {options["dir"]} selected for scanning')
+		root_dir = settings.MEDIA_ROOT + f"/{options['dir']}"
+
 		# Look for the .ini file
 		ini_file = ""
-		for file in os.listdir(settings.MEDIA_ROOT + f"/{options['dir']}"):
-			if os.path.isfile(f"{settings.MEDIA_ROOT}/{file}") and file.endswith(".ini"):
+		for file in os.listdir(root_dir):
+			if os.path.isfile(f"{root_dir}/{file}") and file.endswith(".ini"):
 				ini_file = file
 				break
 		if ini_file == "":
@@ -34,7 +39,7 @@ class Command(BaseCommand):
 
 		# Read .ini file
 		config = configparser.ConfigParser()
-		config.read(f"{settings.MEDIA_ROOT}/{ini_file}")
+		config.read(f"{root_dir}/{ini_file}")
 
 		# Look for person ID section
 		person_section = ""
@@ -59,7 +64,6 @@ class Command(BaseCommand):
 			person_ids[flag] = temp_person
 
 		# Start Processing
-		files_to_update = []
 		for section in config.sections():
 			# Bypass if no faces flag
 			if "faces" not in config[section]:
@@ -67,9 +71,20 @@ class Command(BaseCommand):
 
 			# Bypass if file not exist
 			file_name_chunk = section.split(".")
+
+			# Make Folder Filter
 			dir_q = Q()
 			if options["dir"] != "":
-				dir_q = Q(folder__name=options["dir"])
+				# Process the path
+				folders = options["dir"].split("/")
+				dir_q = Q(folder__name=folders[-1])
+				layer_count = 1
+				for folder in reversed(folders[:-1]):
+					dir_q &= Q(**{
+						f"folder{'__parent'*layer_count}__name": folder
+					})
+			
+			# Query
 			picture = get_or_none(File, dir_q, file_name=".".join(file_name_chunk[:-1]), file_ext=file_name_chunk[-1])
 			if picture is None:
 				self.stdout.write(self.style.ERROR('Could not find the file ' + section))
