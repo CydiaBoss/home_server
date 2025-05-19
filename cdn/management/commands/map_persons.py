2653e1eae9f26bb817c959875fe2d5d078a948d2
Andrew Wang <andrew_wang2002@hotmail.com>
2025-05-19 16:38:31 -0400

    improvements to the mapping

--- cdn/management/commands/map_persons.py
+++ cdn/management/commands/map_persons.py
@@ -3,6 +3,7 @@ from typing import Union
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db.models import Q
 
 from cdn.models import *
 from common.utils import get_or_none, get_media_types
@@ -23,8 +24,8 @@ class Command(BaseCommand):
 
 		# Look for the .ini file
 		ini_file = ""
-		for file in os.listdir(settings.MEDIA_ROOT):
-			if os.path.isfile(file) and file.endswith(".ini"):
+		for file in os.listdir(settings.MEDIA_ROOT + f"/{options['dir']}"):
+			if os.path.isfile(f"{settings.MEDIA_ROOT}/{file}") and file.endswith(".ini"):
 				ini_file = file
 				break
 		if ini_file == "":
@@ -33,14 +34,14 @@ class Command(BaseCommand):
 
 		# Read .ini file
 		config = configparser.ConfigParser()
-		config.read(ini_file)
+		config.read(f"{settings.MEDIA_ROOT}/{ini_file}")
 
 		# Look for person ID section
 		person_section = ""
 		for section in config.sections():
 			if "Contacts" in section:
 				person_section = section
-		if ini_file == "":
+		if person_section == "":
 			self.stdout.write(self.style.ERROR('No section with person ID data found'))
 			return
 
@@ -71,19 +72,31 @@ class Command(BaseCommand):
 
 			# Bypass if file not exist
 			file_name_chunk = section.split(".")
-			picture = get_or_none(File, file_name=".".join(file_name_chunk[:-1]), file_ext=".".join(file_name_chunk[-1]))
+			dir_q = Q()
+			if options["dir"] != "":
+				dir_q |= Q(folder__name=options["dir"])
+			picture = get_or_none(File, dir_q, file_name=".".join(file_name_chunk[:-1]), file_ext=".".join(file_name_chunk[-1]), )
 			if picture is None:
 				self.stdout.write(self.style.ERROR('Could not find the file ' + section))
 				continue
 
 			# Process Each Entry
 			for info in config[section]["face"].split(";"):
-				# TODO maybe issue here
 				picture.tags.add(person_ids[info.split(",")[1]])
+			files_to_update.append(picture)
+
+			# Batching
+			if len(files_to_update) >= options["batch_size"]:
+				self.stdout.write('Bulk saving...')
+				File.objects.bulk_update(files_to_update)
+				files_to_update = []
+
+		# Final push
+		if len(files_to_update) > 0:
+			File.objects.bulk_update(files_to_update)
 
 		self.stdout.write(self.style.SUCCESS('Successfully scanned the MEDIA_ROOT'))
 		
 	def add_arguments(self, parser):
 		parser.add_argument("--batch_size", type=int, help="batch size to use before bulk creating new entries in DB", default=50)
-		parser.add_argument("--max_reads", type=int, help="maximum amount of entries to make", default=-1)
 		parser.add_argument("--dir", help="directory to scan within MEDIA_ROOT", default="")
